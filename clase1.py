7326b0311c5a9ac4d8ccd5403575ae99c7b83f00
Pedro Luis Artunduaga Ortiz <plao2411p@gmail.com>
2020-03-03 15:39:01 -0500

    Primer taller de clase, me funciona correctamente en pyhton2.7

---
+++ clase1.py
@@ -0,0 +1,11 @@
+print("Ejercicio de caida libre")
+print ("g=9.81")
+h=input ("Ingrese la altura desde la cual se deja caer el objeto --->  ")
+t= input ("Ingrese el tiempo en el que desea calcular la alrura a la que se encutra el objeto --->  ")
+t2= float (t)
+h2= float (h)
+g= 9.81
+y= h2-0.5*g*(t**2)
+print ("La altura es ---> ",y)
+
+
